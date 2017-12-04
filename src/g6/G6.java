35816a7d5621ef52d7b193442f5c0dd4354c2fc9
Nicolai <Nicolai@10.50.138.132>
2017-12-04 12:20:09 +0100

    refactor names of temp og g6

--- src/g6/G6.java
+++ src/g6/G6.java
@@ -1,67 +1,32 @@
 package g6;
 
 import battleship.interfaces.BattleshipsPlayer;
-import battleship.interfaces.Board;
-import battleship.interfaces.Fleet;
-import battleship.interfaces.Position;
-import java.util.Random;
-import g6.placers.ShipPlacer;
-import g6.shooters.Shooter;
+import tournament.player.PlayerFactory;
 
-public class G6 implements BattleshipsPlayer {
-
-    private final Random rnd;
-    
-    private Shooter shooter;
-    private ShipPlacer placer;
-    private int round;
+public class G6 implements PlayerFactory<BattleshipsPlayer> {
 
     public G6() {
-        rnd = new Random();
-        shooter = null;
-        placer = null;
-
     }
 
     @Override
-    public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY) {
-        shooter = new Shooter(sizeX, sizeY, rnd);
-        placer = new ShipPlacer(sizeX, sizeY, rnd);
+    public BattleshipsPlayer getNewInstance() {
+        return new Temp();
     }
 
     @Override
-    public void startRound(int round) {
-        this.round = round;
+    public String getID() {
+        return "G6";
     }
 
     @Override
-    public void placeShips(Fleet fleet, Board board) {
-        shooter.newRound(round);
-        placer.placeShips(fleet, board);
+    public String getName() {
+        return "the boys";
     }
 
     @Override
-    public void incoming(Position pos) {
-        placer.incoming(pos);
-    }
-
-    @Override
-    public Position getFireCoordinates(Fleet enemyShips) {
-        return shooter.getFireCoordinates(enemyShips);
-    }
-
-    @Override
-    public void hitFeedBack(boolean hit, Fleet enemyShips) {
-        shooter.hitFeedBack(hit, enemyShips);
-    }
-
-    @Override
-    public void endRound(int round, int points, int enemyPoints) {
-
-    }
-
-    @Override
-    public void endMatch(int won, int lost, int draw) {
-
+    public String[] getAuthors() {
+        String[] res = {"Filip Filipovic, Nicolai Rosenvinge, Jonas Grønbek"};
+                 
+        return res;
     }
 }
